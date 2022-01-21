45f9647b19445db61368e8c5ad53bbe3631a0f10
David P. Chassin <dchassin@slac.stanford.edu>
2022-01-21 13:09:21 -0800

    Update openfido.sh

--- openfido.sh
+++ openfido.sh
@@ -1,33 +1,3 @@
-#!/bin/python3
+#!/bin/sh
 
-try:
-
-    import os, sys, shutil
-
-    sys.path.append(".")
-
-    import weather
-
-    OPENFIDO_OUTPUT = os.getenv("OPENFIDO_OUTPUT")
-
-    CSVFILE="weather.csv"
-    GLMFILE="weather.glm"
-    LATLON=[37.4,-122.2]
-    YEARS=[2020]
-    NAME="test"
-    BRANCH="develop"
-
-    os.chdir("/tmp")
-
-    outputs = [CSVFILE,GLMFILE]
-
-    weather.main([],outputs,{"year":YEARS,"position":LATLON,"name":NAME})
-
-    for RESULT in outputs:
-        shutil.copyfile(RESULT,OPENFIDO_OUTPUT)
-
-except Exception as err:
-
-    print("\n\n*** ABNORMAL TERMINATION ***\nSee error console output for details.")
-    raise
-
+python3 openfido.py
