e6036d2b0dd04058eebec2e7233a7c0655940a32
David P. Chassin <dchassin@slac.stanford.edu>
2022-01-21 14:38:53 -0800

    Update openfido.py

--- openfido.py
+++ openfido.py
@@ -36,15 +36,27 @@ import __init__ as weather
 
 OPENFIDO_INPUT = os.getenv("OPENFIDO_INPUT")
 
-with open(f"{OPENFIDO_INPUT}/config.csv") as f:
-    reader = csv.reader(f)
-    for line in reader:
-        if len(line) == 1:
-            globals()[line[0]] = True
-        elif len(line) == 2:
-            globals()[line[0]] = line[1]
-        elif len(line) > 2:
-            globals()[line[0]] = line[1:]
+try:
+    with open(f"{OPENFIDO_INPUT}/config.csv","r") as f:
+        reader = csv.reader(f)
+        for line in reader:
+            if len(line) == 1:
+                globals()[line[0]] = True
+            elif len(line) == 2:
+                globals()[line[0]] = line[1]
+            elif len(line) > 2:
+                globals()[line[0]] = line[1:]
+except Exception as err:
+    print("ERROR: {err}, template written to output",file=sys.stderr)
+    with open(f"{OPENFIDO_OUTPUT}/config.csv","w") as f:
+        printf("CSVFILE,weatherfile.csv",file=f)
+        printf("GLMFILE,weatherfile.glm,file=f)
+        printf("NAME,glmobjectname,file=f)
+        printf("EMAIL,your.email@your.org,file=f)
+        printf("APIKEY,your-api-key,file=f)
+        printf("YEARS,year1,year2,...,file=f)
+        printf("ATLON,latitude,longitude"),file=f)
+        exit(1)
 
 OPENFIDO_OUTPUT = os.getenv("OPENFIDO_OUTPUT")
 
