1daa41b5c84338f88ec12f7a501c99d51ea3317e
David P. Chassin <dchassin@slac.stanford.edu>
2022-01-21 13:24:15 -0800

    Update openfido.sh

--- openfido.sh
+++ openfido.sh
@@ -5,9 +5,7 @@ set -x # exit on error
 set -u # nounset enabled
 
 apt-get update -y > /dev/stderr
-apt-get install python3 -q -y > /dev/stderr
+apt-get install python3 python3-pip -q -y > /dev/stderr
+python3 -m pip install requests -q
 
-echo "Running in $PWD..." 
-ls $PWD
-
-/usr/bin/python3 openfido.py
+python3 openfido.py
