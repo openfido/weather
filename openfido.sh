770c6b900c51d7d236d68509f892eccf80248b85
David P. Chassin <dchassin@slac.stanford.edu>
2022-01-21 13:25:17 -0800

    Update openfido.sh

--- openfido.sh
+++ openfido.sh
@@ -6,6 +6,6 @@ set -u # nounset enabled
 
 apt-get update -y > /dev/stderr
 apt-get install python3 python3-pip -q -y > /dev/stderr
-python3 -m pip install requests -q
+python3 -m pip install -r requirements.txt -q
 
 python3 openfido.py
